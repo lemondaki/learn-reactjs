d2687d6fe1554b93d14266df7b96acd60ac246b5
Lemondakii <quangductran3009@gmail.com>
2023-03-02 23:56:46 +0700

    change title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi, I'm a dev React
+          Hi, I'm a dev React 10 điểm
         </p>
         <a
           className="App-link"
